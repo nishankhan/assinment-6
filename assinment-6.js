a0e245e7466d11c4cff1c9649e74baa78d380edf
nishankhan <nishankhanwebdevelopment5@gmail.com>
2022-03-02 09:54:03 +0600

    All code

--- assinment-6.js
+++ assinment-6.js
@@ -1,22 +1,26 @@
 const allPhone = () => {
-    const searchValue = document.getElementById('input').value;
-    // console.log(searchValue);
+    const searchValue = document.getElementById('input');
+    const dcgds = searchValue.value;
     searchValue.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+    // console.log(searchValue);
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${dcgds}`;
     fetch(url)
         .then(res => res.json())
         .then(data => showPhone(data.data))
+    searchValue.value = '';
 }
 const showPhone = (phones) => {
     // console.log(phones);    
     const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
     for (const phone of phones) {
         console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-100">
-                     <img src="${phone.image}" class="card-img-top" alt="...">
+                     <img class="w-50 " src="${phone.image}" class="card-img-top" alt="...">
                     <div class="card-body">
                     <h3>Name :${phone.phone_name} </h3>
                     <h3>Brand:${phone.brand} </h3>                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
@@ -26,28 +30,32 @@ const showPhone = (phones) => {
          </div>
         `;
         searchResult.appendChild(div);
+
     }
 }
 const details = (id) => {
     const url = (` https://openapi.programming-hero.com/api/phone/${id}`);
     fetch(url)
         .then(res => res.json())
-        .then(data => displyPhoneDetails(data.data.slug))
+        .then(data => displyPhoneDetails(data.data))
 }
 const displyPhoneDetails = (phone) => {
+
     console.log(phone);
-    const pahoneDetails = document.getElementById('phone-details');
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
-    <img src="${phone.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-            card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-    `;
-    pahoneDetails.appendChild(div)
+    // const pahoneDetails = document.getElementById('phone-details');
+    // // console.log(pahoneDetails, 'hfaeh');
+    // const div = document.createElement('div');
+    // div.classList.add('card');
+    // div.innerHTML = `
+    // <img src="${phone.image}" class="card-img-top" alt="...">
+    // <div class="card-body">
+    // <h3>Name :${phone.phone_name} </h3>
+    // <h3>Brand:${phone.brand} </h3> 
+    //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+    //         card's content.</p>
+    //     <a href="#" class="btn btn-primary">Go somewhere</a>
+    // </div>
+    // `;
+    // pahoneDetails.appendChild(div)
 
 }
